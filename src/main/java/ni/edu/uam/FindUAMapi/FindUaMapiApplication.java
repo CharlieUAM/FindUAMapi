13d4c73b249a9aa8e400173d5324bb51a2ad8af2
CharlieUAM <cramirezl@uamv.edu.ni>
2026-05-27 19:49:32 -0600

    Initial commit

---
+++ src/main/java/ni/edu/uam/FindUAMapi/FindUaMapiApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.FindUAMapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindUaMapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindUaMapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ni/edu/uam/FindUAMapi/FindUaMapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.FindUAMapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindUaMapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
