809868c6b0962d19fddd0dc04b83afcf60038aae
3DG4RDC26 <ercasco@uamv.edu.ni>
2026-06-03 19:40:33 -0600

    Creacion Usuario and UsuarioRepository

---
+++ src/main/java/ni/edu/uam/FindUAMapi/entity/Usuario.java
@@ -0,0 +1,86 @@
+package ni.edu.uam.FindUAMapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Integer idUsuario;
+
+    private String nombre;
+
+    private String apellido;
+
+    @Column(name = "correo_uam")
+    private String correoUam;
+
+    private String telefono;
+
+    private String password;
+
+    @Column(name = "foto_perfil")
+    private String fotoPerfil;
+
+    public Usuario() {
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoUam() {
+        return correoUam;
+    }
+
+    public void setCorreoUam(String correoUam) {
+        this.correoUam = correoUam;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+}
---
+++ src/main/java/ni/edu/uam/FindUAMapi/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package ni.edu.uam.FindUAMapi.repository;
+
+import ni.edu.uam.FindUAMapi.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository
+        extends JpaRepository<Usuario, Integer> {
+}
