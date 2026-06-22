d437dde31dc42659459dec10b7a4582aca103fbf
3DG4RDC26 <ercasco@uamv.edu.ni>
2026-06-21 21:40:34 -0600

    se agrego la posibilidad de editar el user

--- src/main/java/ni/edu/uam/FindUAMapi/controller/UsuarioController.java
+++ src/main/java/ni/edu/uam/FindUAMapi/controller/UsuarioController.java
@@ -56,4 +56,31 @@ public class UsuarioController {
 
         return ResponseEntity.notFound().build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> actualizarUsuario(
+            @PathVariable Integer id,
+            @RequestBody Usuario datosActualizados
+    ) {
+
+        return repository.findById(id)
+                .map(usuario -> {
+
+                    usuario.setNombre(
+                            datosActualizados.getNombre()
+                    );
+
+                    usuario.setApellido(
+                            datosActualizados.getApellido()
+                    );
+
+                    usuario.setTelefono(
+                            datosActualizados.getTelefono()
+                    );
+
+                    return ResponseEntity.ok(
+                            repository.save(usuario)
+                    );
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
